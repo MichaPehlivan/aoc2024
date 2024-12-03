77aaceb8251d3ec0e849d1d2af25d98acea3e967
MichaPehlivan <michapehlivan2004@gmail.com>
2024-12-03 11:49:39 +0100

    day 3

---
+++ src/day3/mod.rs
@@ -0,0 +1,67 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
+use regex::Regex;
+
+
+pub fn solve1() -> usize {
+    let file = File::open("src/day3/input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut total = 0;
+
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").expect("Invalid regex!");
+
+    for line in reader.lines() {
+        let line_clone = line.unwrap().clone();
+
+        for capture in re.captures_iter(&line_clone) {
+            let a = &capture[1];
+            let b = &capture[2];
+            let a_num = usize::from_str_radix(a, 10).unwrap();
+            let b_num = usize::from_str_radix(b, 10).unwrap();
+            total += a_num*b_num;
+        }
+
+    }
+    total
+}
+
+pub fn solve2() -> usize {
+    let file = File::open("src/day3/input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut total = 0;
+    let mut command_vec = vec![];
+
+    let re = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").expect("Invalid regex!");
+
+    for line in reader.lines() {
+        let line_clone = line.unwrap().clone();
+
+        for capture in re.captures_iter(&line_clone) {
+            let matched = capture[0].to_string();
+            command_vec.push(matched);
+        }
+    }
+
+    let remul = Regex::new(r"mul\((\d+),(\d+)\)").expect("Invalid regex!");
+
+    let mut enabled = true;
+    for command in command_vec {
+        match command.as_str() {
+            "don't()" => enabled = false,
+            "do()" => enabled = true,
+            _ => {
+                if enabled {
+                    let capture = remul.captures(&command).unwrap();
+                    let a = &capture[1];
+                    let b = &capture[2];
+                    let a_num = usize::from_str_radix(a, 10).unwrap();
+                    let b_num = usize::from_str_radix(b, 10).unwrap();
+                    total += a_num*b_num;
+                }
+            },
+        }
+    }
+    total
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     let answer1_1 = day1::solve1();
@@ -11,4 +12,9 @@ fn main() {
     let answer2_2 = day2::solve2();
     println!("day 2 answer 1 = {answer1_2}");
     println!("day 2 answer 2 = {answer2_2}");
+
+    let answer1_3 = day3::solve1();
+    let answer2_3 = day3::solve2();
+    println!("day 3 answer 1 = {answer1_3}");
+    println!("day 3 answer 2 = {answer2_3}");
 }
