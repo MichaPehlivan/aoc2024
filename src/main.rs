f25182847e31219b57f29ab4e3b1a9ec35945e7f
MichaPehlivan <michapehlivan2004@gmail.com>
2024-12-07 20:18:56 +0100

    added timing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 mod day1;
 mod day2;
 mod day3;
@@ -7,38 +9,53 @@ mod day6;
 mod day7;
 
 fn main() {
+    let mut now = Instant::now();
+
     let answer1_1 = day1::solve1();
+    println!("day 1 answer 1 = {answer1_1} time: {:?}", now.elapsed());
+    now = Instant::now();
     let answer2_1 = day1::solve2();
-    println!("day 1 answer 1 = {answer1_1}");
-    println!("day 1 answer 2 = {answer2_1}");
+    println!("day 1 answer 2 = {answer2_1} time: {:?}", now.elapsed());
+    now = Instant::now();
 
     let answer1_2 = day2::solve1();
+    println!("day 2 answer 1 = {answer1_2} time: {:?}", now.elapsed());
+    now = Instant::now();
     let answer2_2 = day2::solve2();
-    println!("day 2 answer 1 = {answer1_2}");
-    println!("day 2 answer 2 = {answer2_2}");
+    println!("day 2 answer 2 = {answer2_2} time: {:?}", now.elapsed());
+    now = Instant::now();
 
     let answer1_3 = day3::solve1();
+    println!("day 3 answer 1 = {answer1_3} time: {:?}", now.elapsed());
+    now = Instant::now();
     let answer2_3 = day3::solve2();
-    println!("day 3 answer 1 = {answer1_3}");
-    println!("day 3 answer 2 = {answer2_3}");
+    println!("day 3 answer 2 = {answer2_3} time: {:?}", now.elapsed());
+    now = Instant::now();
 
     let answer1_4 = day4::solve1();
+    println!("day 4 answer 1 = {answer1_4} time: {:?}", now.elapsed());
+    now = Instant::now();
     let answer2_4 = day4::solve2();
-    println!("day 4 answer 1 = {answer1_4}");
-    println!("day 4 answer 2 = {answer2_4}");
+    println!("day 4 answer 2 = {answer2_4} time: {:?}", now.elapsed());
+    now = Instant::now();
 
     let answer1_5 = day5::solve1();
+    println!("day 5 answer 1 = {answer1_5} time: {:?}", now.elapsed());
+    now = Instant::now();
     let answer2_5 = day5::solve2();
-    println!("day 5 answer 1 = {answer1_5}");
-    println!("day 5 answer 2 = {answer2_5}");
+    println!("day 5 answer 2 = {answer2_5} time: {:?}", now.elapsed());
+    now = Instant::now();
 
     let answer1_6 = day6::solve1();
+    println!("day 6 answer 1 = {answer1_6} time: {:?}", now.elapsed());
+    now = Instant::now();
     let answer2_6 = day6::solve2();
-    println!("day 6 answer 1 = {answer1_6}");
-    println!("day 6 answer 2 = {answer2_6}");
+    println!("day 6 answer 2 = {answer2_6} time: {:?}", now.elapsed());
+    now = Instant::now();
 
     let answer1_7 = day7::solve1();
+    println!("day 7 answer 1 = {answer1_7} time: {:?}", now.elapsed());
+    now = Instant::now();
     let answer2_7 = day7::solve2();
-    println!("day 7 answer 1 = {answer1_7}");
-    println!("day 7 answer 2 = {answer2_7}");
+    println!("day 7 answer 2 = {answer2_7} time: {:?}", now.elapsed());
 }
