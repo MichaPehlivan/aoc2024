0285d69d0eecd3da592548fae6bfaef29cc4dac8
MichaPehlivan <michapehlivan2004@gmail.com>
2024-12-13 22:31:32 +0100

    faster day 13

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -6,12 +6,12 @@ pub fn solve1() -> f64 {
     let file = File::open("src/day13/input.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let mut inputs: Vec<((isize, isize), (isize, isize), (isize, isize))> = vec![];
+    let mut inputs: Vec<((f64, f64), (f64, f64), (f64, f64))> = vec![];
 
     let mut total: f64 = 0_f64;
 
-    let mut button_a = (0, 0);
-    let mut button_b = (0, 0);
+    let mut button_a = (0_f64, 0_f64);
+    let mut button_b = (0_f64, 0_f64);
     let mut prize;
 
     let re_ab = Regex::new(r"X\+(\d+), Y\+(\d+)").expect("Invalid regex!");
@@ -22,20 +22,20 @@ pub fn solve1() -> f64 {
 
         if line_clone.starts_with("Button A") {
             let capture = re_ab.captures(&line_clone).unwrap();
-            let x = isize::from_str_radix(&capture[1], 10).unwrap();
-            let y = isize::from_str_radix(&capture[2], 10).unwrap();
+            let x = capture[1].parse().unwrap();
+            let y = capture[2].parse().unwrap();
             button_a = (x, y);
         }
         else if line_clone.starts_with("Button B") {
             let capture = re_ab.captures(&line_clone).unwrap();
-            let x = isize::from_str_radix(&capture[1], 10).unwrap();
-            let y = isize::from_str_radix(&capture[2], 10).unwrap();
+            let x = capture[1].parse().unwrap();
+            let y = capture[2].parse().unwrap();
             button_b = (x, y);
         }
         else if line_clone.starts_with("Prize") {
             let capture = re_p.captures(&line_clone).unwrap();
-            let x = isize::from_str_radix(&capture[1], 10).unwrap();
-            let y = isize::from_str_radix(&capture[2], 10).unwrap();
+            let x = capture[1].parse().unwrap();
+            let y = capture[2].parse().unwrap();
             prize = (x, y);
             inputs.push((button_a, button_b, prize));
         }
@@ -44,13 +44,12 @@ pub fn solve1() -> f64 {
     for ((a, c), (b, d), (p, q)) in inputs {
         let den = a*d-b*c;
 
-        if den != 0 {
-            let numx: f64 = ((d*p-b*q) as f64)/(den as f64);
-            let numy: f64 = ((a*q-c*p) as f64)/(den as f64);
+        if den != 0_f64 {
+            let numx: f64 = (d*p-b*q)/den;
+            let numy: f64 = (a*q-c*p)/den;
 
-            if numx > 0_f64 && numy > 0_f64 && numx.fract() == 0_f64 && numy.fract() == 0_f64 {
+            if numx.fract() == 0_f64 && numy.fract() == 0_f64 {
                 let price = 3_f64*numx + numy;
-                println!("price: {price}, A {numx} times + B {numy} times");
                 total += price;
             }
         }
@@ -62,12 +61,12 @@ pub fn solve2() -> f64 {
     let file = File::open("src/day13/input.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let mut inputs: Vec<((isize, isize), (isize, isize), (isize, isize))> = vec![];
+    let mut inputs: Vec<((f64, f64), (f64, f64), (f64, f64))> = vec![];
 
     let mut total: f64 = 0_f64;
 
-    let mut button_a = (0, 0);
-    let mut button_b = (0, 0);
+    let mut button_a = (0_f64, 0_f64);
+    let mut button_b = (0_f64, 0_f64);
     let mut prize;
 
     let re_ab = Regex::new(r"X\+(\d+), Y\+(\d+)").expect("Invalid regex!");
@@ -78,21 +77,21 @@ pub fn solve2() -> f64 {
 
         if line_clone.starts_with("Button A") {
             let capture = re_ab.captures(&line_clone).unwrap();
-            let x = isize::from_str_radix(&capture[1], 10).unwrap();
-            let y = isize::from_str_radix(&capture[2], 10).unwrap();
+            let x = capture[1].parse().unwrap();
+            let y = capture[2].parse().unwrap();
             button_a = (x, y);
         }
         else if line_clone.starts_with("Button B") {
             let capture = re_ab.captures(&line_clone).unwrap();
-            let x = isize::from_str_radix(&capture[1], 10).unwrap();
-            let y = isize::from_str_radix(&capture[2], 10).unwrap();
+            let x = capture[1].parse().unwrap();
+            let y = capture[2].parse().unwrap();
             button_b = (x, y);
         }
         else if line_clone.starts_with("Prize") {
             let capture = re_p.captures(&line_clone).unwrap();
-            let x = isize::from_str_radix(&capture[1], 10).unwrap();
-            let y = isize::from_str_radix(&capture[2], 10).unwrap();
-            prize = (x+10000000000000, y+10000000000000);
+            let x: f64 = capture[1].parse().unwrap();
+            let y: f64 = capture[2].parse().unwrap();
+            prize = (x+10000000000000_f64, y+10000000000000_f64);
             inputs.push((button_a, button_b, prize));
         }
     }
@@ -100,11 +99,11 @@ pub fn solve2() -> f64 {
     for ((a, c), (b, d), (p, q)) in inputs {
         let den = a*d-b*c;
 
-        if den != 0 {
-            let numx: f64 = ((d*p-b*q) as f64)/(den as f64);
-            let numy: f64 = ((a*q-c*p) as f64)/(den as f64);
+        if den != 0_f64 {
+            let numx: f64 = (d*p-b*q)/den;
+            let numy: f64 = (a*q-c*p)/den;
 
-            if numx > 0_f64 && numy > 0_f64 && numx.fract() == 0_f64 && numy.fract() == 0_f64 {
+            if numx.fract() == 0_f64 && numy.fract() == 0_f64 {
                 let price = 3_f64*numx + numy;
                 total += price;
             }
